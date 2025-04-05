b90954c85ce208c61d00a9c7b01d33a081072334
Danik2023 <danik.kyrteu2@gmail.com>
2025-04-05 20:50:51 +0300

    Update contactsControllers.js

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,6 +1,13 @@
 import contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = (req, res) => {
+  try {
+    contacts = contactsService.listContacts();
+    res.status(200).json(contacts);
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving contacts" });
+  }
+};
 
 export const getOneContact = (req, res) => {};
 
