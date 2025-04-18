39ffa5cfe320564865ce95668686c8f1d582a2dc
Danik2023 <danik.kyrteu2@gmail.com>
2025-04-18 13:09:55 +0300

    ...

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-
 import contactsRouter from "./routes/contactsRouter.js";
+import mongoose from "mongoose";
 
 const app = express();
+
+const DB_HOST =
+  "mongodb+srv://Danik:StZk9R_v3ajVS.-@cluster0.uhqjmpt.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("The database is successfully connected");
+    app.listen(3000, () => {
+      console.log("Server is running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log("Database connection error:", error.message);
+    process.exit(1);
+  });
 
 app.use(morgan("tiny"));
 app.use(cors());
@@ -19,10 +35,6 @@ app.use((_, res) => {
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
-});
-
-app.listen(3000, () => {
-  console.log("Server is runnidvdcdcdcng. Use our API on port: 3000");
 });
 
 //StZk9R_v3ajVS.-
