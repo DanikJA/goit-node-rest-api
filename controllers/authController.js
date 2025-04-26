4828c8c5e85fc1e3f9b3ba227599bbb7a5ca40f9
Danik2023 <danik.kyrteu2@gmail.com>
2025-04-26 19:13:40 +0300

    ...

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,13 @@
 import { User } from "../models/user.js";
-
+import HttpError from "../helpers/HttpError.js";
 export const register = async (req, res, next) => {
   try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user) {
+      throw HttpError(409, "This email is already in use");
+    }
     const newUser = await User.create(req.body);
     res.status(201).json({
       email: newUser.email,
