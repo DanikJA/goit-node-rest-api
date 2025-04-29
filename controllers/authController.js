7f5bc2d882d0c4f7686273fb722d105f416a6e5b
Danik2023 <danik.kyrteu2@gmail.com>
2025-04-29 23:36:03 +0300

    ...

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,19 @@
 import { User } from "../models/user.js";
 import HttpError from "../helpers/HttpError.js";
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
+
+const { SECRET_KEY } = process.env;
+
+// try {
+//   const { id } = jwt.verify(token, SECRET_KEY);
+//   console.log("Verified user id:", id);
+// } catch (error) {
+//   console.log("JWT error:", error.message);
+// }
 
 export const register = async (req, res, next) => {
   try {
@@ -17,6 +30,31 @@ export const register = async (req, res, next) => {
     res.status(201).json({
       email: newUser.email,
       name: newUser.name,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw HttpError(401, "Email or password is invalid");
+    }
+
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      throw HttpError(401, "Email or password is invalid");
+    }
+
+    const payload = { id: user._id };
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+
+    res.json({
+      token,
     });
   } catch (error) {
     next(error);
--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,10 @@ import express from "express";
 import isValidId from "../middleware.js";
 import schemas from "../models/user.js";
 import validateBody from "../helpers/validateBody.js";
-import { register } from "../controllers/authController.js";
+import { register, login } from "../controllers/authController.js";
 
 const router = express.Router();
 router.post("/register", validateBody(schemas.registerSchema), register);
-router.post("/login", validateBody(schemas.loginSchema), register);
+router.post("/login", validateBody(schemas.loginSchema), login);
 
 export default router;
